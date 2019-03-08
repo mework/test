c493a33632472e15b6e225a2f623fbf6783a0293
canmeng <448088477@qq.com>
2019-03-08 17:16:07 +0800

    init

---
+++ test/index.spec.js
@@ -0,0 +1,6 @@
+describe('函数测试', function() {
+	it('add函数测试', function() {
+		const add2 = add(2);
+		expect(add2(4)).toBe(6);
+	})
+})
---
+++ web/index.js
@@ -0,0 +1,5 @@
+function add(num) {
+	return function(val) {
+		return val + num;
+	}
+}
